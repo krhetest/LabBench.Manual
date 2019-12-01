8e1f76f8bf18145fd66cec68788a78115760ddd7
Kristian Hennings <krhe@inventors.dk>
2019-12-01 19:07:06 +0100

    Updated functions for plotting data

--- analysis/labAnalysis/labTE_PlotChannel.m
+++ analysis/labAnalysis/labTE_PlotChannel.m
@@ -1,6 +1,5 @@
-function [d] = labTE_PlotChannel(R, chan)
+function labTE_PlotChannel(R, chan)
 % labTE_PlotChannel Plot a single channel from a threshold estimation test
-
 d = R.ch{chan};
 N = 1:length(d.response);
 I1 = find(d.response == 1);
@@ -10,13 +9,13 @@ figure(1);
 clf;
 set(gcf, 'Color', [1 1 1]);
 
-plot(I0, d.intensity(I0), 'r.',...
-     I1, d.intensity(I1), 'b.',...
+plot(I0, d.intensity(I0), 'r*',...
+     I1, d.intensity(I1), 'b*',...
      [1 length(d.response)],d.threshold * d.Imax * [1 1],'k',...
      N, d.function.alpha .* d.Imax,'b-');
 text(length(d.response), R.THR(chan), sprintf('%.2fmA', R.THR(chan)));
 legend('Not Felt','Felt','Threshold', 'Alpha', 'Location','southeast','Orientation','horizontal');
-title(d.ID);
+title(sprintf('%s (%s)', R.id, d.ID));
 set(gca,'Box','off');
 set(gca,'TickDir','out');
 xlabel('Trials []');
